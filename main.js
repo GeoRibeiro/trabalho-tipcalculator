55d438eeca8f9f8f51e7ecec49053a0d72a1ebbe
GeoRibeiro <geogeomorais.lavander@gmail.com>
2025-04-02 23:50:32 -0300

    inicio dos ajustes oara receber a porcentagem da gorjeta escolhida

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let tipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
  let billInput = document.querySelector("#bill")
  billInput.addEventListener("input", receiveBillValue)
@@ -11,13 +12,17 @@ numberOfPeopleInput.addEventListener("input", )
  function receiveBillValue(){
     bill = billInput.valueAsNumber
 
-    calculate()
+    calculate() a paula ta se ferrando aq
 }
 
 function receiveNumberOfPeopleValue(){
     numberOfPeople = numberOfPeopleInput.valueAsNumber
 
     calculate()
+}
+
+function receiveTipPercentageValue(value){
+    buttonSelected = document.querySelector("")
 }
 
 function calculate(){
